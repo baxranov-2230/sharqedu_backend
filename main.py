c829ba4f576f0eaefa471099399a0394018d9ad2
baxranov-2230 <axrorwillberich2230@gmail.com>
2025-05-01 08:13:16 +0500

    xscsd

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from src.api import main_router
 import uvicorn
 
-
 app = FastAPI()
-
 
 app.include_router(main_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+    ],
+)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app" , reload=True)
+    uvicorn.run("main:app", reload=True)
