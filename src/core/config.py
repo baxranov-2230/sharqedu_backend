c3bd662b7051342d73b84d8ea4aed9ac84948054
baxranov-2230 <axrorwillberich2230@gmail.com>
2025-05-08 19:26:32 +0500

    one alembic

--- src/core/config.py
+++ src/core/config.py
@@ -1,36 +1,32 @@
-from pydantic_settings import BaseSettings , SettingsConfigDict
+import os
 
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv()
 
 class Settings(BaseSettings):
-    DB_USER: str 
-    DB_HOST: str 
-    DB_PASSWORD: str 
-    DB_PORT: str 
-    DB_NAME: str 
-    MODE: str
+    DB_USER: str = os.getenv('DB_USER')
+    DB_HOST: str = os.getenv('DB_HOST')
+    DB_PASSWORD: str = os.getenv('DB_PASSWORD')
+    DB_PORT: str = os.getenv('DB_PORT')
+    DB_NAME: str = os.getenv('DB_NAME')
 
-    ACCESS_SECRET_KEY: str
-    REFRESH_SECRET_KEY : str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    REFRESH_TOKEN_EXPIRE_DAYS: int
-    ALGORITHM: str
-
+    ACCESS_SECRET_KEY: str=os.getenv('ACCESS_SECRET_KEY')
+    REFRESH_SECRET_KEY: str=os.getenv('REFRESH_SECRET_KEY')
+    ACCESS_TOKEN_EXPIRE_MINUTES: int=os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+    REFRESH_TOKEN_EXPIRE_DAYS: int=os.getenv('REFRESH_TOKEN_EXPIRE_DAYS')
+    ALGORITHM: str=os.getenv('ALGORITHM')
 
 
-
-
-    
     @property
     def connection_string(self):
-        return (
-            f'postgresql+asyncpg://'
-            f'{self.DB_USER}:'
-            f'{self.DB_PASSWORD}@'
-            f'{self.DB_HOST}:{self.DB_PORT}/'
-            f'{self.DB_NAME}'
-        )
+        values=self.model_dump()
+        return (f'postgresql+asyncpg://'
+                f'{values["DB_USER"]}:'
+                f'{values["DB_PASSWORD"]}@'
+                f'{values["DB_HOST"]}:{values["DB_PORT"]}/'
+                f'{values["DB_NAME"]}')
 
 
-    model_config = SettingsConfigDict(env_file=".env")
-
-settings = Settings()
+settings=Settings()
