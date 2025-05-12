06b3ff5568bcebd34d10a9e8fe490786397aa1d1
baxranov-2230 <axrorwillberich2230@gmail.com>
2025-05-12 08:12:25 +0500

    Allow Ip

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-from fastapi import FastAPI
+
+
+from fastapi import FastAPI, Request, HTTPException
+from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.middleware.cors import CORSMiddleware
 
 from src.api import main_router
 import uvicorn
+ALLOWED_IP = "192.168.1.116"
 
 app = FastAPI(
     # docs_url=None,  # Swagger UI ni o'chirish
     # redoc_url=None  # ReDoc ni o'chirish
 )
+
+@app.get("/real-ip")
+async def get_real_ip(request: Request):
+    forwarded_for = request.headers.get("x-forwarded-for")
+    if forwarded_for:
+        ip = forwarded_for.split(",")[0]
+    else:
+        ip = request.client.host
+    return {"ip": ip}
+
+
+
+class SwaggerAccessMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        if request.url.path.startswith("/docs") or request.url.path.startswith("/openapi.json"):
+            client_ip = request.client.host
+            if client_ip != ALLOWED_IP:
+                raise HTTPException(status_code=403, detail="Swagger sahifasiga kirishga ruxsat yo'q")
+        return await call_next(request)
+
+app.add_middleware(SwaggerAccessMiddleware)
+
+
 
 app.include_router(main_router)
 
